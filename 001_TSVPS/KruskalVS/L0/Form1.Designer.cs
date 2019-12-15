4227ce466163b0ee831ab9dad67b3a4c777936da
Stepan <stepan-alekseev-87@mail.ru>
2019-12-15 18:42:49 +0700

    Kruscal, Deikstra, Ford-Bellman are done in a cool way
    Former-commit-id: 30df40497f3ecde51d662865c4db1bcd035a0d7e

--- 001_TSVPS/KruskalVS/L0/Form1.Designer.cs
+++ 001_TSVPS/KruskalVS/L0/Form1.Designer.cs
@@ -57,11 +57,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button7 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -124,6 +129,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(201, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 1;
@@ -153,6 +163,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 58);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 70;
             this.dataGridView1.Size = new System.Drawing.Size(471, 278);
             this.dataGridView1.TabIndex = 6;
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellValueChanged);
@@ -171,9 +182,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(12, 389);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "расстояния по Беллману-Форду";
+            this.label4.Text = "Беллман-Форд";
             // 
             // label5
             // 
@@ -189,22 +200,22 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(12, 437);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.Size = new System.Drawing.Size(13, 13);
             this.label6.TabIndex = 12;
-            this.label6.Text = "антисимметричность";
+            this.label6.Text = "а";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(12, 467);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.Size = new System.Drawing.Size(12, 13);
             this.label7.TabIndex = 13;
-            this.label7.Text = "транзитивность";
+            this.label7.Text = "т";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(408, 30);
+            this.button3.Location = new System.Drawing.Point(408, 29);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 15;
@@ -214,16 +225,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 355);
+            this.textBox1.Location = new System.Drawing.Point(150, 355);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(594, 20);
+            this.textBox1.Size = new System.Drawing.Size(645, 20);
             this.textBox1.TabIndex = 16;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(201, 382);
+            this.textBox2.Location = new System.Drawing.Point(150, 382);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(594, 20);
+            this.textBox2.Size = new System.Drawing.Size(645, 20);
             this.textBox2.TabIndex = 17;
             // 
             // textBox3
@@ -249,7 +260,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(499, 207);
+            this.button2.Location = new System.Drawing.Point(489, 29);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 22;
@@ -259,7 +270,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(580, 207);
+            this.button4.Location = new System.Drawing.Point(570, 29);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 23;
@@ -270,7 +281,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(489, 32);
+            this.label2.Location = new System.Drawing.Point(494, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(301, 117);
             this.label2.TabIndex = 25;
@@ -284,34 +295,24 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(499, 262);
+            this.textBox7.Location = new System.Drawing.Point(785, 262);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(296, 20);
+            this.textBox7.Size = new System.Drawing.Size(10, 20);
             this.textBox7.TabIndex = 27;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(496, 233);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(193, 13);
-            this.label9.TabIndex = 28;
-            this.label9.Text = "Задать свои вершины через пробел ";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(720, 288);
+            this.button6.Location = new System.Drawing.Point(785, 288);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.Size = new System.Drawing.Size(10, 23);
             this.button6.TabIndex = 29;
-            this.button6.Text = "Задать";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(499, 319);
+            this.checkBox1.Location = new System.Drawing.Point(583, 330);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(129, 17);
             this.checkBox1.TabIndex = 31;
@@ -330,7 +331,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(499, 288);
+            this.button5.Location = new System.Drawing.Point(489, 202);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(88, 23);
             this.button5.TabIndex = 33;
@@ -338,17 +339,78 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.Button5_Click_1);
             // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(731, 183);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(50, 20);
+            this.textBox6.TabIndex = 34;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(489, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(236, 13);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "номер исх. вершины для поиска расстояний:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(327, 6);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(169, 17);
+            this.checkBox2.TabIndex = 36;
+            this.checkBox2.Text = "считываем матрицу из кода";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(489, 231);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 23);
+            this.button8.TabIndex = 37;
+            this.button8.Text = "Deikstra";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(651, 29);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(130, 23);
+            this.button9.TabIndex = 38;
+            this.button9.Text = "Запомнить матрицу";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(502, 6);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(199, 17);
+            this.checkBox3.TabIndex = 39;
+            this.checkBox3.Text = "считываем запомненную матрицу";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(807, 493);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
@@ -409,11 +471,16 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.CheckBox checkBox3;
     }
 }
 
