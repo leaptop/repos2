7f9359522cb42f13cb9a09aa053dc6fa9822218b
Stepan <stepan-alekseev-87@mail.ru>
2020-01-02 19:32:29 +0700

    before movung of db funcs to mainwindow

--- 004_CHMV/QTLearning/CW01/main.cpp
+++ 004_CHMV/QTLearning/CW01/main.cpp
@@ -2,11 +2,12 @@
 
 #include <QApplication>
 #include <QtSql>
+#include<QTableView>
 
 static bool createConnection()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("addressbook");
+    db.setDatabaseName("journal");
 
     db.setUserName("elton");
     db.setHostName("epica");
@@ -24,19 +25,13 @@ int main(int argc, char *argv[])
 
     if (!createConnection()) {
         return -1;
-    }else
-        QSqlQuery query;
+    }
 
-   // if()
     //Creating of the data base
     QSqlQuery query;
-   // QString str0 = "CREATE TABLE IF NOT EXISTS";
-   // if(!query.exec())
-    QString   str  = "CREATE TABLE IF NOT EXISTS addressbook ( "
-                         "number INTEGER PRIMARY KEY NOT NULL, "
-                         "name   VARCHAR(15), "
-                         "phone  VARCHAR(12), "
-                         "email  VARCHAR(15) "
+    QString   str  = "CREATE TABLE IF NOT EXISTS mainTable ( "
+                         "name TEXT, "
+                         "data TEXT"
                      ");";
 
     if (!query.exec(str)) {
@@ -45,51 +40,64 @@ int main(int argc, char *argv[])
 
     //Adding some information
     QString strF =
-          "INSERT INTO  addressbook (number, name, phone, email) "
-          "VALUES(%1, '%2', '%3', '%4');";
+          "INSERT INTO  mainTable (name, data) "
+          "VALUES('%1', '%2');";
 
-    str = strF.arg("3")
-              .arg("Shtefan")
-              .arg("+7 913 389 51 27")
-              .arg("stnAlv@mega.de");
+    str = strF.arg("My first planer 02.01.2019 ")
+              .arg("Something is happening");
+
     if (!query.exec(str)) {
         qDebug() << "Unable to make insert opeation";
     }
 
-    str = strF.arg("4")
-              .arg("Lora")
-              .arg("+45 7845 654")
-              .arg("lora@mega.de");
-    if (!query.exec(str)) {
-        qDebug() << "Unable to make insert operation";
-    }
+//    str = strF.arg("4")
+//              .arg("Lora")
+//              .arg("+45 7845 654")
+//              .arg("lora@mega.de");
+//    if (!query.exec(str)) {
+//        qDebug() << "Unable to make insert operation";
+//    }
 
-    if (!query.exec("SELECT * FROM addressbook;")) {
+    if (!query.exec("SELECT * FROM mainTable;")) {
         qDebug() << "Unable to execute query - exiting";
         return 1;
     }
 
+
     //Reading of the data
-    QSqlRecord rec     = query.record();
-    int        nNumber = 0;
-    QString    strName;
-    QString    strPhone;
-    QString    strEmail;
+//    QSqlRecord rec     = query.record();
+//    int        nNumber = 0;
+//    QString    strName;
+//    QString    strPhone;
+//    QString    strEmail;
 
-    while (query.next()) {
-        nNumber  = query.value(rec.indexOf("number")).toInt();
-        strName  = query.value(rec.indexOf("name")).toString();
-        strPhone = query.value(rec.indexOf("phone")).toString();
-        strEmail = query.value(rec.indexOf("email")).toString();
+//    while (query.next()) {
+//        nNumber  = query.value(rec.indexOf("number")).toInt();
+//        strName  = query.value(rec.indexOf("name")).toString();
+//        strPhone = query.value(rec.indexOf("phone")).toString();
+//        strEmail = query.value(rec.indexOf("email")).toString();
 
-        qDebug() << nNumber << " " << strName << ";\t"
-                 << strPhone << ";\t" << strEmail;
-    }
+//        qDebug() << nNumber << " " << strName << ";\t"
+//                 << strPhone << ";\t" << strEmail;
+//    }
+
+
+    QTableView     view;
+    QSqlTableModel model;
+
+    model.setTable("mainTable");
+    model.select();
+    model.setEditStrategy(QSqlTableModel::OnFieldChange);
+
+    view.setModel(&model);
+    view.resize(450, 100);
+    view.show();
 
 
 
 
     MainWindow w;
+
     w.show();
     return a.exec();
 }
