3eeebac50aaa340778a8ca8deaf5a3b94f763a3e
Stepan <stepan-alekseev-87@mail.ru>
2020-01-03 16:14:11 +0700

    It all stopped working
    Former-commit-id: 041469de9d8c93fdab831ba5738cd16c73f93675

--- 004_CHMV/QTLearning/CW01/main.cpp
+++ 004_CHMV/QTLearning/CW01/main.cpp
@@ -23,22 +23,24 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    if (!createConnection()) {
-        return -1;
-    }
+   // if (!createConnection()) { return -1;  }
 
-    QTableView     view;
-    QSqlTableModel model0;
+//    QTableView     view;
+//    QSqlTableModel model0;
 
-    model0.setTable("mainTable");
-    model0.setSort(0, Qt::DescendingOrder);
-    model0.setFilter("id  = '2'");
-    model0.select();
-    model0.setEditStrategy(QSqlTableModel::OnFieldChange);
+//    model0.setTable("mainTable");//seems like its impossible to choose certain fields,
+//    //using QSqlTableModel...
+//    model0.setSort(0, Qt::DescendingOrder);//this is how to sort
+//    model0.setFilter("id  = '2'");//this is how to filter
+//    //model0->setQuery("SELECT name FROM mainTable");//doesn't work
+//    //model0.selectStatement()//not sure how it works
+//    model0.select();
+//    model0.setEditStrategy(QSqlTableModel::OnFieldChange);
 
-    view.setModel(&model0);
-    view.resize(450, 100);
-    view.show();
+//    view.setModel(&model0);
+//    view.hideColumn(0);//but its possible to hide them here
+//    view.resize(450, 100);
+//    view.show();
 
 
 
