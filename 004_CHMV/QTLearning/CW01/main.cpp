113aad4701c3d6c881fcaa3cc4ce2be621fc8961
Stepan <stepan-alekseev-87@mail.ru>
2020-01-06 23:02:48 +0700

    Implemented styles normally

--- 004_CHMV/QTLearning/CW01/main.cpp
+++ 004_CHMV/QTLearning/CW01/main.cpp
@@ -13,34 +13,30 @@ void loadModules(QSplashScreen* psplash)
             time.start();
             ++i;
         }
-        psplash->showMessage("Loading modules: "
-                             +QString::number(i)+"%",
-                             Qt::AlignCenter|Qt::AlignCenter,
-                             Qt::black
-                             );
+        psplash->showMessage("Loading modules: " +QString::number(i)+"%",
+                             Qt::AlignCenter|Qt::AlignCenter, Qt::black);
         qApp->processEvents();
     }
 }
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //StyleLoader::attach();//uncommenting it allows to change style momentarily by pressing F5
+    //StyleLoader::attach();//uncomment to apply changes from a qss file momentarily(F5)
     //at the same time input in textEdit etc fileds becomes impossible
     //style.qss is in build/debug
 
-    QFile file(":/styles/Common/style.qss");//this is how to use resource files
-    qDebug()<<QFile::exists("./../../CW01/style.qss");//still learning how to search files
     // qDebug()<<QFile::exists("./qu.txt");//RETURNS TRUE IF THE FILE IS IN SOURCE DIRECTORY
+    QFile file(":/styles/Common/style.qss");//this is how to use resource files
     file.open(QFile::ReadOnly);
     QString strCSS = QLatin1String(file.readAll());
-    //qApp->setStyleSheet(strCSS);
+    //qApp->setStyleSheet(strCSS);//uncomment to apply changes from a qss file
 
-//    QSplashScreen splash(QPixmap(":/pics/Common/s.png"));
-//    splash.show();
-//    loadModules(&splash);
+    //    QSplashScreen splash(QPixmap(":/pics/Common/s.png"));//uncomment to turn zastavka on
+    //    splash.show();
+    //    loadModules(&splash);
 
     MainWindow w;
-  //  splash.finish(&w);
+    //  splash.finish(&w);//uncomment to turn zastavka on
 
     w.show();
     return a.exec();
