9a49224da11e74a872129eb12afe55f8ac098cd6
stepan <stepan-alekseev-87@mail.ru>
2020-02-08 13:06:07 +0700

    -abefore the pair of GPU
    Former-commit-id: 0195e11566ab9360669f51ead9cb12ed2b3f2dbf

--- gpu/lab1_01/lab1_01/kernel.cu
+++ gpu/lab1_01/lab1_01/kernel.cu
@@ -7,6 +7,7 @@
 #include <Windows.h>
 #include <time.h> 
 #include <iostream>
+#include <fstream>
 //определил kernel для device
 __global__ void sum(int* a, int* b, int N) {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;//если координаты потока в блоке потоков в сумме с координатами блока потоков в сетке,
@@ -14,305 +15,115 @@ __global__ void sum(int* a, int* b, int N) {
 	if (i >= N) return;
 	a[i] += b[i];
 }
-int main() {
-	//выбрал device
-	cudaSetDevice(0);//установил девайс для исполнения ГПУ команд
-	//выделил память на device
-	int N = 0;//размер векторов
-	int* src_a, * src_b; int* dev_a, * dev_b;
-	/*
-	__host____device__cudaError_t cudaMalloc (void **devPtr, size_t size) Allocate memory on the device.
-Parameters: devPtr - Pointer to allocated DEVICE memory; size - Requested allocation size in bytes
-Returns cudaSuccess, cudaErrorInvalidValue, cudaErrorMemoryAllocation
-Description: Allocates size bytes of linear memory on the device and returns in *devPtr a pointer to the allocated memory.
-The allocated memory is suitably aligned for any kind of variable. The memory is not cleared.
-cudaMalloc() returns cudaErrorMemoryAllocation in case of failure.
-Modules
-www.nvidia.com CUDA Runtime API vRelease Version | 127
-The device version of cudaFree cannot be used with a *devPtr allocated using the host API, and vice versa.
-	*/
-	cudaMalloc(&dev_a, sizeof(int) * N);//выделил память на девайсе. //sizeof(int) = 4. Короче выделил памяти на N интов(1024)
-	cudaMalloc(&dev_b, sizeof(int) * N);//Второй параметр - размер в байтах. причём память не обнулённая
+int N = 0;//размер векторов
+int* src_a, * src_b; int* dev_a, * dev_b;
+LARGE_INTEGER t1, t2, f, t3, t4, t5, t6, t7, t8;
+int* blocksPerGrid_gl, * threadsPerBlock_gl, * N_gl, * time_gl;
+int i_gl =1;
+int num = 15;
+void allocateMemory(int N) {
+	cudaMalloc(&dev_a, sizeof(int) * N);
+	cudaMalloc(&dev_b, sizeof(int) * N);
 	src_a = (int*)malloc(sizeof(int) * N); src_b = (int*)malloc(sizeof(int) * N);
-	//for (int i = 0; i < N; i++) { src_a[i] = i; src_b[i] = N - i; }
 	for (int i = 0; i < N; i++) { src_a[i] = rand(); src_b[i] = rand(); }
-	//следующая функция копирует данные между хостом и девайсом:(в нашем случае с хоста на девайс)
-	//Первый параметр - место назначения, второй - источник,
-	//третий - размер для копирования в байтах, четвертый - тип передачи.
-	/*
-	__host__cudaError_t cudaMemcpy
-	(void *dst, const void *src, size_t count, cudaMemcpyKind kind)
-Копирует count байт из памяти, на которую указывает src в память, на которую указывает
-dst. kind определяет направление копирования и может быть только: cudaMemcpyHostToHost, cudaMemcpyHostToDevice, cudaMemcpyDeviceToHost,
-cudaMemcpyDeviceToDevice, or cudaMemcpyDefault. Рекомендовано использовать cudaMemcpyDefault, при котором направление определяется
-(угадывается) по значениям указателей.
-	*/
-	/*
-	__host__cudaError_t cudaMemcpy (void *dst, const void *src, size_t count, cudaMemcpyKind kind) Copies data between host and device.
-Parameters dst - Destination memory address src - Source memory address count - Size in bytes to copy kind - Type of transfer
-Modules
-www.nvidia.com CUDA Runtime API vRelease Version | 144
-Returns cudaSuccess, cudaErrorInvalidValue, cudaErrorInvalidMemcpyDirection
-Description: Copies ##count## bytes from the memory area pointed to by ###src### to the memory area pointed to by ###dst###,
-where ##kind## specifies the direction of the copy, and must be one of cudaMemcpyHostToHost, cudaMemcpyHostToDevice, cudaMemcpyDeviceToHost,
-cudaMemcpyDeviceToDevice, or cudaMemcpyDefault. Passing cudaMemcpyDefault is recommended, in which case the type of transfer
-is inferred from the pointer values. However, cudaMemcpyDefault is only allowed on systems that support unified
-virtual addressing. Calling cudaMemcpy() with dst and src pointers that do not match the direction of the copy results
-in an undefined behavior.
-	*/
-	////скопировал данные с host на device:
-	cudaMemcpy(dev_a, src_a, sizeof(int) * N, cudaMemcpyHostToDevice);//выделяю память
+	cudaMemcpy(dev_a, src_a, sizeof(int) * N, cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_b, src_b, sizeof(int) * N, cudaMemcpyHostToDevice);
-
-	LARGE_INTEGER t1, t2, f, t3, t4, t5, t6, t7, t8;
-	
-	//QueryPerformanceFrequency(&f);
-	//double tm = double(t2.QuadPart - t1.QuadPart) / f.QuadPart;// так можно уже в секундах вывести
-	//std::cout << "\nFirstTime \n" << tm;
-
-	// Launch the Vector Add CUDA Kernel
-	int threadsPerBlock = 1;
+}
+void launchKernel(int N, int threadsPerBlock, int * time_gl, int * N_gl ) {
 	int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
 	QueryPerformanceCounter(&t3);//максимальное число тредов(второй параметр) - 1024
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
+	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);
 	cudaDeviceSynchronize();
 	QueryPerformanceCounter(&t4);
 	double tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	std::cout << "threadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	time_gl[i_gl] = tm;
+	N_gl[i_gl] = N; i_gl++;
+}
+void testFunction() {
+	int threadsPerblock_local = 1;
+	std::ofstream out;          // поток для записи
+	out.open("C:\\Users\\stepa\\repos2\\gpu\\lab1_01\\lab1_01\\results.txt"); // окрываем файл для записи
 
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 2;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	for (int i_thr = threadsPerblock_local; i_thr < 1024; i_thr *= 2)
+	{
+		if (out.is_open())
+		{
+			out << "\n\nA new test: " << std::endl;
+		}
+		N_gl = (int*)std::malloc(num * sizeof(int));
+		time_gl = (int*)std::malloc(num * sizeof(int));
+		//для  числа тредов: ...
+		for (int i_N = 1 << 10; i_N <= 1 << 23; i_N *= 2)//доходим до максимального числа потоков// всё таки по заданию пройтись надо по размерам векторов
+		{//при этом также взять разные конфигурации нитей... пусть их будут 3
+			allocateMemory(i_N);// по заданию размер массива может быть от 1<<10 до 1<<23
+			launchKernel(i_N, i_thr, time_gl, N_gl);
+		}
+		std::cout << "\n\narrSize N   time , threadsPerblock_local = " << i_thr;
+		out << std::endl << "arrSizeN  time , threadsPerblock_local = " << i_thr;
+		for (int ipi = 1; ipi < num; ipi++)
+		{
+			std::cout << std::endl << N_gl[ipi] << "    " << time_gl[ipi];
+			out << std::endl << N_gl[ipi] << "     " << time_gl[ipi];
+		}
+		free(N_gl);
+		free(time_gl);
+		i_gl = 1;
+		
+	}
+}
+int main() {
+	cudaSetDevice(0);//установил девайс для исполнения ГПУ команд
+	int size0 = 1024;//макс число нитей?
+	testFunction();
+	/*std::ofstream out;          // поток для записи
+	out.open("C:\\Users\\stepa\\repos2\\gpu\\lab1_01\\lab1_01\\results.txt"); // окрываем файл для записи
+	if (out.is_open())
+	{
+		out << "A new test: " << size0 << std::endl;
+	}
+	N_gl = (int*)std::malloc(num * sizeof(int));
+	time_gl = (int*)std::malloc(num * sizeof(int));
+	//для  числа тредов: 256
+	for (int i_N = 1  << 10; i_N < 1 << 23; i_N *= 2)//доходим до максимального числа потоков// всё таки по заданию пройтись надо по размерам векторов
+	{//при этом также взять разные конфигурации нитей... пусть их будут 3
+		allocateMemory(i_N);// по заданию размер массива может быть от 1<<10 до 1<<23
+		launchKernel(i_N, 256, time_gl,  N_gl);
+	}
+	std::cout << "size N   time ";
+	for (int ipi = 0; ipi < num; ipi++)
+	{
+		std::cout << 256 <<"    " <<   time_gl[ipi] << std::endl;
+	}
+	//для числа тредов: 512
+	for (int i_N = 1 << 10; i_N < 1 << 23; i_N *= 2)//доходим до максимального числа потоков// всё таки по заданию пройтись надо по размерам векторов
+	{//при этом также взять разные конфигурации нитей... пусть их будут 3
+		allocateMemory(i_N);// по заданию размер массива может быть от 1<<10 до 1<<23
+		launchKernel(i_N, 512, time_gl, N_gl);
+	}
+	std::cout << "size N   time ";
+	for (int ipi = 0; ipi < num; ipi++)
+	{
+		std::cout << 512 << "    " << time_gl[ipi] << std::endl;
+	}
+	//для числа тредов: 1024
+	for (int i_N = 1 << 10; i_N < 1 << 23; i_N *= 2)//доходим до максимального числа потоков// всё таки по заданию пройтись надо по размерам векторов
+	{//при этом также взять разные конфигурации нитей... пусть их будут 3
+		allocateMemory(i_N);// по заданию размер массива может быть от 1<<10 до 1<<23
+		launchKernel(i_N, 1024, time_gl, N_gl);
+	}
+	std::cout << "size N   time ";
+	for (int ipi = 0; ipi < num; ipi++)
+	{
+		std::cout << 1024 << "    " << time_gl[ipi] << std::endl;
+	}
 
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 4;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	//int ff = 1 << 23;
+	//std::cout << "ff = " << ff;
+	//int a = 16 << 1;//с константами работает
+	//std::cout << "\na = " << a;
 
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 8;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	*/
+}
 
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 16;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
-
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 32;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
-
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 64;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
-
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 128;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
-
-	// Launch the Vector Add CUDA Kernel
-	 threadsPerBlock = 256;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t3);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t4);
-	tm = double(t4.QuadPart - t3.QuadPart);// / f.QuadPart;
-	std::cout << "\nthreadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
-
-	 threadsPerBlock = 1024;
-	 blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	QueryPerformanceCounter(&t5);
-	sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);//http://ecee.colorado.edu/~siewerts/extra/code/example_code_archive/a490dmis_code/CUDA/cuda_work/samples/0_Simple/vectorAdd/vectorAdd.cu
-	cudaDeviceSynchronize();
-	QueryPerformanceCounter(&t6);
-	tm = double(t6.QuadPart - t5.QuadPart);// / f.QuadPart;
-	std::cout <<" time: " << tm << "\n";
-
-	// // Launch a kernel on the GPU with one thread for each element.
-	//    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);//size здесь - это моё N
-
-		/*
-		__host____device__cudaError_t cudaDeviceSynchronize (void) Wait for compute device to finish.
-	Returns cudaSuccess
-	Description: Blocks until the device has completed all preceding requested tasks.
-	cudaDeviceSynchronize() returns an error if one of the preceding tasks has failed.
-	If the cudaDeviceScheduleBlockingSync flag was set for this device, the host thread will block until the device has finished its work.
-
-		*/
-	cudaDeviceSynchronize();
-	cudaMemcpy(src_a, dev_a, sizeof(int) * N, cudaMemcpyDeviceToHost);
-
-
-	//printf("sizeof(int) = %d", sizeof(int));
-	//for (int i = 0; i < N; i++)//just to assure, that it works
-	//	printf("\nsrc_a[%d] = %d", i, src_a[i]);
-
-	
- }
-//
-//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-//
-//__global__ void addKernel(int *c, const int *a, const int *b)
-//{
-//    int i = threadIdx.x;
-//    c[i] = a[i] + b[i];
-//}
-//
-//int main()
-//{
-//    const int arraySize = 5;
-//    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-//    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-//    int c[arraySize] = { 0 };
-//
-//    // Add vectors in parallel.
-//    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "addWithCuda failed!");
-//        return 1;
-//    }
-//
-//    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-//        c[0], c[1], c[2], c[3], c[4]);
-//
-//    // cudaDeviceReset must be called before exiting in order for profiling and
-//    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-//    cudaStatus = cudaDeviceReset();
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaDeviceReset failed!");
-//        return 1;
-//    }
-//
-//    return 0;
-//}
-//
-//// Helper function for using CUDA to add vectors in parallel.
-//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-//{
-//    int *dev_a = 0;
-//    int *dev_b = 0;
-//    int *dev_c = 0;
-//    cudaError_t cudaStatus;
-//
-//    // Choose which GPU to run on, change this on a multi-GPU system.
-//    cudaStatus = cudaSetDevice(0);
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-//        goto Error;
-//    }
-//
-//    // Allocate GPU buffers for three vectors (two input, one output)    .
-//    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMalloc failed!");
-//        goto Error;
-//    }
-//
-//    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMalloc failed!");
-//        goto Error;
-//    }
-//
-//    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMalloc failed!");
-//        goto Error;
-//    }
-//
-//    // Copy input vectors from host memory to GPU buffers.
-//    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMemcpy failed!");
-//        goto Error;
-//    }
-//
-//    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMemcpy failed!");
-//        goto Error;
-//    }
-//
-//    // Launch a kernel on the GPU with one thread for each element.
-//    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-//
-//    // Check for any errors launching the kernel
-//    cudaStatus = cudaGetLastError();
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-//        goto Error;
-//    }
-//    
-//    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-//    // any errors encountered during the launch.
-//    cudaStatus = cudaDeviceSynchronize();
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-//        goto Error;
-//    }
-//
-//    // Copy output vector from GPU buffer to host memory.
-//    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-//    if (cudaStatus != cudaSuccess) {
-//        fprintf(stderr, "cudaMemcpy failed!");
-//        goto Error;
-//    }
-//
-//Error:
-//    cudaFree(dev_c);
-//    cudaFree(dev_a);
-//    cudaFree(dev_b);
-//    
-//    return cudaStatus;
-//}
