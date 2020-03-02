f5cc419eb3667491fd305f38b4359b755f2e97db
stepan <stepan-alekseev-87@mail.ru>
2020-03-02 21:52:15 +0700

    Networks CHAP & IP-addresses configurated. Before VLAN configuration
    Former-commit-id: d256395982f2fd03fe1e5a1dc5bc97261110aa90

--- gpu/lab3_00/lab3_00/kernel.cu
+++ gpu/lab3_00/lab3_00/kernel.cu
@@ -4,6 +4,12 @@
 
 #include <stdio.h>
 
+#include <cuda.h>
+#include <Windows.h>
+#include <time.h> 
+#include <iostream>
+#include <fstream>
+
 cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
 
 __global__ void addKernel(int* c, const int* a, const int* b)
@@ -27,31 +33,101 @@ __global__ void gTest2(float* a) {
 	int J = gridDim.y * blockDim.y;
 	a[j + i * J] = (float)(threadIdx.y + threadIdx.x * blockDim.y);
 }
+int n = 1000000;
+float* a;
+
+int N = 0;//размер векторов
+int* src_a, * src_b; int* dev_a, * dev_b;
+LARGE_INTEGER t1, t2, f, t3, t4, t5, t6, t7, t8;
+int* blocksPerGrid_gl, * threadsPerBlock_gl, * N_gl;
+double* time_gl;
+int i_gl = 1;
+int num = 15;
+void allocateMemory(int n) {
+	cudaMalloc(&a, sizeof(float) * n);
+	a = (float*)malloc(sizeof(float) * n);
+	//src_b = (int*)malloc(sizeof(int) * N);
+	for (int i = 0; i < n; i++) { a[i] = rand(); // b[i] = rand(); 
+	}
+}
+void launchKernel(int N, int threadsPerBlock, double* time_gl, int* N_gl) {
+	int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+	printf("\nCUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+	QueryPerformanceCounter(&t3);//максимальное число тредов(второй параметр) - 1024
+	//sum << <blocksPerGrid, threadsPerBlock >> > (dev_a, dev_b, N);
+	gTest1 << <blocksPerGrid, threadsPerBlock >> > (a);
+	cudaDeviceSynchronize();
+	QueryPerformanceCounter(&t4);
+	double tm = double((t4.QuadPart - t3.QuadPart) / (double)2600);// / f.QuadPart;// TRANSLATING TACTS TO MILLISECONDS
+//	std::cout << "threadsPerBlock: " << threadsPerBlock << ", time: " << tm << "\n";
+	time_gl[i_gl] = tm;
+	N_gl[i_gl] = N; i_gl++;
+}
+void testFunction() {
+	int threadsPerblock_local = 1;
+	std::ofstream out;          // поток дл€ записи
+
+	//out.open("C:\\Users\\stepa\\repos2\\gpu\\lab1_01\\lab1_01\\results.txt"); // окрываем файл дл€ записи
+	out.open("..\\results.txt"); // окрываем файл дл€ записи
+
+	for (int i_thr = threadsPerblock_local; i_thr < 1024; i_thr *= 2)//графиков д.б. столько, сколько у мен€ конфигураций нитей - 10
+	{
+		if (out.is_open())
+		{
+			//out << "\n\n" << std::endl;
+		}
+		N_gl = (int*)std::malloc(num * sizeof(int));
+		time_gl = (double*)std::malloc(num * sizeof(double));
+		//дл€  числа тредов: ...
+		for (int i_N = 1 << 10; i_N <= 1 << 23; i_N *= 2)//доходим до максимального числа потоков// всЄ таки по заданию пройтись надо по размерам векторов
+		{//при этом также вз€ть разные конфигурации нитей... пусть их будут 3
+			allocateMemory(i_N);// по заданию размер массива может быть от 1<<10 до 1<<23
+			launchKernel(i_N, i_thr, time_gl, N_gl);
+		}
+		//std::cout << "\n\narrSize N   time , threadsPerblock_local = " << i_thr;
+		//out << std::endl << "arrSizeN time threadsPerblock_local=" << i_thr;
+		//for (int ipi = 1; ipi < num; ipi++)
+		//{
+		//	std::cout << std::endl << N_gl[ipi] << "    " << time_gl[ipi];
+		//	out << std::endl << N_gl[ipi] << " " << time_gl[ipi];
+		//}
+		free(N_gl);
+		free(time_gl);
+		i_gl = 1;
+
+	}
+}
 
 int main()
 {
-	const int arraySize = 5;
+	cudaSetDevice(0);//установил девайс дл€ исполнени€ √ѕ” команд
+	int size0 = 1024;//макс число нитей?
+	//allocateMemory(n);
+
+	testFunction();
+
+	/*const int arraySize = 5;
 	const int a[arraySize] = { 1, 2, 3, 4, 5 };
 	const int b[arraySize] = { 10, 20, 30, 40, 50 };
-	int c[arraySize] = { 0 };
+	int c[arraySize] = { 0 };*/
 
 	// Add vectors in parallel.
-	cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+	/*cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "addWithCuda failed!");
 		return 1;
-	}
+	}*/
 
-	printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-		c[0], c[1], c[2], c[3], c[4]);
+	//printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+		//c[0], c[1], c[2], c[3], c[4]);
 
 	// cudaDeviceReset must be called before exiting in order for profiling and
 	// tracing tools such as Nsight and Visual Profiler to show complete traces.
-	cudaStatus = cudaDeviceReset();
+	/*cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaDeviceReset failed!");
 		return 1;
-	}
+	}*/
 
 	return 0;
 }
@@ -104,7 +180,7 @@ cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
 	}
 
 	// Launch a kernel on the GPU with one thread for each element.
-	addKernel << <1, size >> > (dev_c, dev_a, dev_b);
+	//addKernel << <1, size >> > (dev_c, dev_a, dev_b);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
