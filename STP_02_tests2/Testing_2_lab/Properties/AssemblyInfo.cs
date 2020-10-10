278a491e5b82652a6e8fe85d0155429eb96ef830
stepan <stepan-alekseev-87@mail.ru>
2020-10-11 01:42:01 +0700

    finished second lab stp tests2
    Former-commit-id: c4aef32379d1ad8941582931d6931f9d68dce60c

---
+++ STP_02_tests2/Testing_2_lab/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing_2_lab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing_2_lab")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3cd5ce6-8fd7-4928-95cb-7164b8b5fc36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
