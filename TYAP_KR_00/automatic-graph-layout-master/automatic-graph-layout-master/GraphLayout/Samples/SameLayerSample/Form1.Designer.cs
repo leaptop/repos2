30798cb38b26f834b41cf6d4e62aaba95920ab61
stepan <stepan-alekseev-87@mail.ru>
2020-11-23 21:55:08 +0700

    everything is done in the TYAP course work  except for the main algorithm...
    Former-commit-id: 944f872556b4fd6d5527df7db2085c655a864f4b

--- TYAP_KR_00/automatic-graph-layout-master/automatic-graph-layout-master/GraphLayout/Samples/SameLayerSample/Form1.Designer.cs
+++ TYAP_KR_00/automatic-graph-layout-master/automatic-graph-layout-master/GraphLayout/Samples/SameLayerSample/Form1.Designer.cs
@@ -42,6 +42,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1Kratnost)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -121,7 +123,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 167);
+            this.button1.Location = new System.Drawing.Point(12, 167);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(107, 23);
             this.button1.TabIndex = 10;
@@ -156,7 +158,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 196);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 60;
-            this.dataGridView1.Size = new System.Drawing.Size(691, 433);
+            this.dataGridView1.Size = new System.Drawing.Size(684, 187);
             this.dataGridView1.TabIndex = 13;
             // 
             // richTextBox1
@@ -167,11 +169,31 @@
             this.richTextBox1.TabIndex = 14;
             this.richTextBox1.Text = "";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(452, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Проверить цепочку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 389);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(684, 271);
+            this.richTextBox2.TabIndex = 16;
+            this.richTextBox2.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1892, 672);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
@@ -211,6 +233,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
 
