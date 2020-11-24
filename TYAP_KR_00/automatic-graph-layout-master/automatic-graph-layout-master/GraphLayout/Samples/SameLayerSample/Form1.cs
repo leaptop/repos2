cac41cc78844a725ae5f99dd7a945f942f7a4a88
stepan <stepan-alekseev-87@mail.ru>
2020-11-24 22:03:07 +0700

    TYAP course work in progress

--- TYAP_KR_00/automatic-graph-layout-master/automatic-graph-layout-master/GraphLayout/Samples/SameLayerSample/Form1.cs
+++ TYAP_KR_00/automatic-graph-layout-master/automatic-graph-layout-master/GraphLayout/Samples/SameLayerSample/Form1.cs
@@ -1,12 +1,12 @@
 ﻿
-using Microsoft.Msagl.Drawing;
-using Microsoft.Msagl.GraphViewerGdi;
-using Microsoft.Msagl.Layout.Layered;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.GraphViewerGdi;
+using Microsoft.Msagl.Layout.Layered;
 
 namespace SameLayerSample {
     public partial class Form1 : Form {
@@ -14,9 +14,9 @@ namespace SameLayerSample {
             InitializeComponent();
             formForTheInputs = this;
 
-            initializeTestCase_abcdefg_3a_bfg_();
+            BuildDFAFirstCase_multiplicityNotEqualToZeroAndTheSymbolIsntInTheFinalSubstring();
 
-            buildDFA();
+            // buildFirstDFA_AndDatagridviewByIt();
 
             //CreatClusteredLayout();
         }
@@ -30,22 +30,89 @@ namespace SameLayerSample {
         string initialState = "q0";
         string finalState = "";
         string currentState = "";
+        Graph graph;
+        IEnumerable<string> alphabetInIEnumerable;
 
-        public void initializeTestCase_abcdefg_3a_bfg_() {
-            textBox1StringToCheck.Text = "bcadacabfg";
-            textBox2Alphabet.Text = "a b c d e f g";
-            textBox3FinalSubString.Text = "bfg";
-            textBox4SymbolForKratnost.Text = "a";
-            numericUpDown1Kratnost.Value = 3;
-
+        public string setExceptParameter(string set, string str) {//returns a string, for example: 
+            //set.Except(str) or {a, b, c}/b = {a, c} = "a c". I.e. the returned string is "a c".
+            string[] aa = new string[1];
+            aa[0] = str;
+            string[] setInArray = set.Split(' ');
+            IEnumerable<string> setWithout_str = setInArray.Except(aa);
+            string setWithout_a_InString = "";
+            foreach (object v in setWithout_str) {
+                setWithout_a_InString += v + " ";
+            }
+            return setWithout_a_InString;
         }
-        public void buildDFA() {
+        public void BuildDFAFirstCase_multiplicityNotEqualToZeroAndTheSymbolIsntInTheFinalSubstring() {
+            //need to have intersections and not equal sets...
+            initializeTestCase_abcdefg_3a_bfg_();
+            readInformationFromTheInterface();
+
+            string a = textBox4SymbolForMultiplicity.Text;
+
+            int multiplicity = (int)numericUpDown1Multiplicity.Value;
+
+            string alphabetInString = textBox2Alphabet.Text;
+
+            string setWithout_a_InString = setExceptParameter(alphabetInString, a);
+
+            GViewer gViewer = new GViewer() { Dock = DockStyle.Fill };
+            SuspendLayout();
+            Controls.Add(gViewer);
+            ResumeLayout();
+            graph = new Graph();
+            var sugiyamaSettings = (SugiyamaLayoutSettings)graph.LayoutAlgorithmSettings;
+            sugiyamaSettings.NodeSeparation *= 2;
+
+
+            for (int i = 0; i < multiplicity; i++) {
+                graph.AddEdge("q" + i, "q" + i).LabelText = setWithout_a_InString;
+                graph.AddEdge("q" + i, "q" + (i + 1)).LabelText = a;
+            }
+            string[] finalSubStringInArray = stringToArrayOfStrings(finalSubstring);
+            for (int i = multiplicity; i < multiplicity + finalSubStringInArray.Length; i++) {
+                graph.AddEdge("q" + i, "q" + (i + 1)).LabelText = finalSubStringInArray[i - multiplicity];
+                graph.AddEdge("q" + i, "q1").LabelText = a;
+            }
+            graph.AddEdge("q" + (multiplicity + finalSubStringInArray.Length), "q1").LabelText = a;
+
+            string[] bb = new string[1];
+            bb[0] = finalSubstring.Substring(0, 1);
+
+            string setWithout_a_and_b_InString = setExceptParameter(setWithout_a_InString, bb[0]);
+
+            graph.AddEdge("q" + multiplicity, "q" + multiplicity).LabelText = setWithout_a_and_b_InString;
+            graph.AddEdge("q" + (multiplicity + 1), "q" + (multiplicity + 1)).LabelText = bb[0];
+
+            if (finalSubstring.Length > 1) {//in general it can be redone by just adding edges with a set of set/LabelText for each next state
+
+                string f = finalSubstring.Substring(1, 1);
+                string setWithout_a_and_b_and_f_InString = setExceptParameter(setWithout_a_and_b_InString, f);
+
+                graph.AddEdge("q" + (multiplicity + 1), "q" + (multiplicity)).LabelText = setWithout_a_and_b_and_f_InString;
+                for (int i = multiplicity + 2; i < finalSubStringInArray.Length + multiplicity + 1; i++) {
+                    graph.AddEdge("q" + i, "q" + (multiplicity + 1)).LabelText = bb[0];
+                }
+            }
+
+
+            graph.Attr.OptimizeLabelPositions = true;
+            graph.Attr.SimpleStretch = true;
+            gViewer.Graph = graph;
+
+            buildDataGridView1ByGraph();
+        }
+
+        public void buildFirstDFA_AndDatagridviewByIt() {
+            initializeTestCase_abcdefg_3a_bfg_();
             readInformationFromTheInterface();
             GViewer gViewer = new GViewer() { Dock = DockStyle.Fill };
             SuspendLayout();
             Controls.Add(gViewer);
             ResumeLayout();
-            Graph graph = new Graph();
+            graph = new Graph();
             var sugiyamaSettings = (SugiyamaLayoutSettings)graph.LayoutAlgorithmSettings;
             sugiyamaSettings.NodeSeparation *= 2;
             graph.AddEdge("q0", "q0").LabelText = "b c d e f g";
@@ -76,68 +143,19 @@ namespace SameLayerSample {
             IEnumerable<Edge> listOfEdges = graph.Edges;
 
 
-            dataGridView1.RowCount = dataGridView1.ColumnCount = graph.NodeCount;
 
-            int p = 0;
-            Hashtable hash = graph.NodeMap;
-            // Edge edge = graph.EdgeById("q0q1");
 
-            List<string> lst = new List<string>();//normally hashtable isn't sorted, so I had to make a list for sorting
-            foreach (var key2 in hash.Keys) {
-                lst.Add(key2.ToString());
-            }
-            lst.Sort();
-            string lastNode = "";
-            foreach (var item in lst) {
-                dataGridView1.Columns[p].HeaderCell.Value = dataGridView1.Rows[p++].HeaderCell.Value = item;
-                finalState = lastNode = item;
-            }
-
-            foreach (var node in graph.Nodes) {//apparently it's easier to build graph by a table, not vice versa... but I'm lazy...
-                foreach (Edge edge in node.Edges) {
-                    for (int i = 0; i < dataGridView1.Rows.Count; i++) {
-                        for (int j = 0; j < dataGridView1.Columns.Count; j++) {
-                            if (edge.Source.Equals(dataGridView1.Rows[i].HeaderCell.Value) &&
-                                edge.Target.Equals(dataGridView1.Columns[j].HeaderCell.Value)) {
-                                dataGridView1.Rows[i].Cells[j].Value = edge.LabelText;
-                            }
-                        }
-                    }
-                }
-            }
-
+            buildDataGridView1ByGraph();
 
             for (int i = 0; i < graph.NodeCount; i++) {
                 //dataGridView1.Columns[i].HeaderCell.Value = graph.
             }
 
 
-            foreach (DictionaryEntry de in hash) {
-                richTextBox1.AppendText(de.Key + "\n\n" + de.Value + "\n\n\n");
-            }
-            foreach (var Node in graph.Nodes) {
-                Node.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Circle;
-                if (Node.Attr.Id.Equals(lastNode)) {
-                    Node.Attr.Shape = Microsoft.Msagl.Drawing.Shape.DoubleCircle;
-                    //Node.Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
-                }
-            }
-            foreach (var node in graph.Nodes) {
-                richTextBox1.AppendText("node.Id.ToString() = " + node.Id.ToString() + "\n");
-                foreach (Edge edge in node.Edges) {
-                    richTextBox1.AppendText("edge.LabelText = " + edge.LabelText + "\n");
-                }
-            }
-            foreach (var item in hash) {
-                richTextBox1.AppendText(" item.GetType() = " + item.GetType() + "\n");
-                richTextBox1.AppendText(" item.GetHashCode() = " + item.GetHashCode() + "\n");
-                richTextBox1.AppendText(" item.ToString() = " + item.ToString() + "\n\n");
-            }
         }
         public void checkAStringByDataGridview1() {
             richTextBox2.Clear();
-            stringToCheck = textBox1StringToCheck.Text;
-            alphabet = textBox2Alphabet.ToString().Split(' ');
+            readInformationFromTheInterface();
             stringTocheckInArrayOfStrings = stringToArrayOfStrings(stringToCheck);
             currentState = initialState;
             for (int i = 0; i < stringTocheckInArrayOfStrings.Length; i++) {
@@ -149,10 +167,9 @@ namespace SameLayerSample {
                                 for (int m = 0; m < symbolsOfCurrentCell.Length; m++) {
                                     if (currentState.Equals(dataGridView1.Rows[j].HeaderCell.Value) &&
                                 stringTocheckInArrayOfStrings[i].Equals(symbolsOfCurrentCell[m])) {
-                                        richTextBox2.AppendText("Текущее состояние: " + currentState + ", ");
+                                        richTextBox2.AppendText(currentState + " -> ");
                                         currentState = dataGridView1.Columns[k].HeaderText;
-                                        richTextBox2.AppendText("новое состояние: " + currentState + ", перешли по символу: " + symbolsOfCurrentCell[m] +
-                                            ", остаток цепочки: ");
+                                        richTextBox2.AppendText(currentState + " (" + symbolsOfCurrentCell[m] + ") ");
                                         for (int h = i + 1; h < stringTocheckInArrayOfStrings.Length; h++) {
                                             if (!(h == stringTocheckInArrayOfStrings.Length))
                                                 richTextBox2.AppendText(stringTocheckInArrayOfStrings[h]);
@@ -184,6 +201,74 @@ namespace SameLayerSample {
                 richTextBox2.AppendText("Находимся в состоянии, отличном от финального, цепочка не принята\n");
             }
         }
+        public void buildDataGridView1ByGraph() {
+            dataGridView1.RowCount = dataGridView1.ColumnCount = graph.NodeCount;
+
+            int p = 0;
+            Hashtable hash = graph.NodeMap;
+            // Edge edge = graph.EdgeById("q0q1");
+
+            List<string> lst = new List<string>();//normally hashtable isn't sorted, so I had to make a list for sorting
+            foreach (var key2 in hash.Keys) {
+                lst.Add(key2.ToString());
+            }
+            lst.Sort();
+            string lastNode = "";
+            foreach (var item in lst) {
+                dataGridView1.Columns[p].HeaderCell.Value = dataGridView1.Rows[p++].HeaderCell.Value = item;
+                finalState = lastNode = item;
+            }
+            foreach (var node in graph.Nodes) {//apparently it's easier to build graph by a table, not vice versa... but I'm lazy...
+                foreach (Edge edge in node.Edges) {//building a datagridview by graph
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++) {
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++) {
+                            if (edge.Source.Equals(dataGridView1.Rows[i].HeaderCell.Value) &&
+                                edge.Target.Equals(dataGridView1.Columns[j].HeaderCell.Value)) {
+                                dataGridView1.Rows[i].Cells[j].Value = edge.LabelText;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (DictionaryEntry de in hash) {
+                richTextBox1.AppendText(de.Key + "\n\n" + de.Value + "\n\n\n");
+            }
+            foreach (var Node in graph.Nodes) {
+                Node.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Circle;
+                if (Node.Attr.Id.Equals(lastNode)) {
+                    Node.Attr.Shape = Microsoft.Msagl.Drawing.Shape.DoubleCircle;
+                    //Node.Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+                }
+            }
+            foreach (var node in graph.Nodes) {
+                richTextBox1.AppendText("node.Id.ToString() = " + node.Id.ToString() + "\n");
+                foreach (Edge edge in node.Edges) {
+                    richTextBox1.AppendText("edge.LabelText = " + edge.LabelText + "\n");
+                }
+            }
+            foreach (var item in hash) {
+                richTextBox1.AppendText(" item.GetType() = " + item.GetType() + "\n");
+                richTextBox1.AppendText(" item.GetHashCode() = " + item.GetHashCode() + "\n");
+                richTextBox1.AppendText(" item.ToString() = " + item.ToString() + "\n\n");
+            }
+        }
+
+        public void readInformationFromTheInterface() {
+            stringToCheck = textBox1StringToCheck.Text;
+            finalSubstring = textBox3FinalSubString.Text;
+            symbolKratnost = textBox4SymbolForMultiplicity.Text;
+            kratnost = (int)numericUpDown1Multiplicity.Value;
+            alphabet = textBox2Alphabet.Text.Split(' ');
+        }
+        public void initializeTestCase_abcdefg_3a_bfg_() {
+            textBox1StringToCheck.Text = "bcadacabfg";
+            textBox2Alphabet.Text = "a b c d e f g";
+            textBox3FinalSubString.Text = "bfg";
+            textBox4SymbolForMultiplicity.Text = "a";
+            numericUpDown1Multiplicity.Value = 3;
+
+        }
         public string[] stringToArrayOfStrings(string stringToCheck) {
             char[] stringTocheckInChars = stringToCheck.ToCharArray();
             string[] stringToCheckInStringArray = new string[stringTocheckInChars.Length];
@@ -192,25 +277,44 @@ namespace SameLayerSample {
             }
             return stringToCheckInStringArray;
         }
-        public void readInformationFromTheInterface() {
-            stringToCheck = textBox1StringToCheck.Text;
-            finalSubstring = textBox3FinalSubString.Text;
-            symbolKratnost = textBox4SymbolForKratnost.Text;
-            kratnost = (int)numericUpDown1Kratnost.Value;
-            alphabet = textBox2Alphabet.Text.Split(' ');
-        }
-
         private void button2_Click(object sender, System.EventArgs e) {
             initializeTestCase_abcdefg_3a_bfg_();
         }
 
         private void button1_Click(object sender, System.EventArgs e) {
             // formForTheGraph = new Form2ForGraph(formForTheInputs);
-            buildDFA();
+            buildFirstDFA_AndDatagridviewByIt();
         }
 
         private void button3_Click(object sender, System.EventArgs e) {
             checkAStringByDataGridview1();
+        }
+
+        private void заданиеToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            MessageBox.Show("Вариант № 1\n      Написать программу, которая по предложенному описанию языка построит " +
+                "детерминированный конечный автомат, распознающий этот язык, " +
+"и проверит вводимые с клавиатуры цепочки на их принадлежность языку. " +
+"Предусмотреть возможность поэтапного отображения на экране " +
+"процесса проверки цепочек. Функция переходов ДКА может изображаться в виде таблицы и " +
+"графа(выбор вида отображения посредством меню). Вариант(первый) задания языка:\n\n" +
+"       Алфавит, обязательная конечная подцепочка всех цепочек языка " +
+"и кратность вхождения выбранного символа алфавита в любую цепочку языка.");
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            MessageBox.Show("Алексеев Степан Владимрович. Группа ИП-712. Ноябрь 2020");
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            System.IO.File.WriteAllText(@"C:\Users\stepa\repos2\TYAP_KR_00\automatic-graph-layout-master\automatic-graph-layout-master\GraphLayout\Samples\SameLayerSample\WriteText.txt", richTextBox2.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            BuildDFAFirstCase_multiplicityNotEqualToZeroAndTheSymbolIsntInTheFinalSubstring();
         }
     }
 }
