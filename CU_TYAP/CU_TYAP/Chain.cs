250d5fdacca3f440f2fc88b784a0da03eca679ac
Stepan <stepan-alekseev-87@mail.ru>
2020-11-30 15:58:44 +0700

    reverse enfineered CU_TYAP
    Former-commit-id: 96c7c29bb93e7b4a40128e8ca7986c8b11c76cb1

---
+++ CU_TYAP/CU_TYAP/Chain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CU_TYAP
+{
+    public class Chain
+    {
+        public string chain;
+        public Chain genericChain;
+        public Chain(string chain, Chain generic = null)
+        {
+            this.chain = chain;
+            genericChain = generic;
+        }
+        public string ShowChain()
+        {
+            Chain ch = this;
+            string res = "";
+            while (ch != null)
+            {
+                res = ch.chain + (ch != this ? "  " : "") + res;
+                ch = ch.genericChain;
+            }
+            return res;
+        }
+        public bool CheckRepeatedGenerates()
+        {
+            bool result = false;
+            Chain ch = this.genericChain;
+            while (ch != null)
+            {
+                if (ch.chain == this.chain)
+                    return true;
+                ch = ch.genericChain;
+            }
+            return result;
+        }
+    }
+}
